edba810372d66088f0ad5d259fa050328437166e
harshinit2025ece-t <harshini.t2025ece@sece.ac.in>
2026-06-05 19:39:53 +0530

    Update Bankacc.java

--- Bankacc.java
+++ Bankacc.java
@@ -12,12 +12,12 @@ class Bankacc {
 
     public static void main(String[] args) {
         Bankacc a1 = new Bankacc();
-        a1.accountNumber = 1001;
-        a1.balance = 15000;
+        a1.accountNumber = 10025;
+        a1.balance = 10000;
 
         Bankacc a2 = new Bankacc();
-        a2.accountNumber = 1002;
-        a2.balance = 25000;
+        a2.accountNumber = 10035;
+        a2.balance = 3000;
 
         a1.display();
         a2.display();
